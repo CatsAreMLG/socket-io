705694afe98b5b888d4bb3a1e5dd8c45b302852d
CatsAreMLG <joshuadgon@gmail.com>
2019-02-26 15:29:25 -0600

    Init commit

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const app = express();
+const http = require("http").Server(app);
+const io = require("socket.io")(http);
+
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.use("/static", express.static("static"));
+
+let num = 0;
+
+io.on("connection", function(socket) {
+  console.log("a user connected");
+  socket.on("disconnect", function() {
+    console.log("user disconnected");
+  });
+  socket.on("increment", function() {
+    num += 1;
+    io.emit("increment", num);
+  });
+  socket.on("decrement", function() {
+    num -= 1;
+    io.emit("decrement", num);
+  });
+  socket.on("draw", function(mouseX, mouseY) {
+    socket.broadcast.emit("draw", mouseX, mouseY);
+  });
+});
+
+http.listen(3000, function() {
+  console.log("listening on *:3000");
+});
