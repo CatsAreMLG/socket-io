541de03e594ae9980ce7262ab74702648f909510
CatsAreMLG <joshuadgon@gmail.com>
2019-02-28 15:01:17 -0600

    Remove p5js

--- static/js/numberControl.js
+++ static/js/numberControl.js
@@ -1,33 +1,15 @@
-var socket = io();
-document.querySelector("#plus").addEventListener("click", e => {
-  e.preventDefault();
-  socket.emit("increment");
-});
-document.querySelector("#minus").addEventListener("click", e => {
-  e.preventDefault();
-  socket.emit("decrement");
-});
-socket.on("increment", function(num) {
-  document.querySelector("#number").innerText = num;
-});
-socket.on("decrement", function(num) {
-  document.querySelector("#number").innerText = num;
-});
-
-function setup() {
-  createCanvas(640, 480);
-}
-
-function draw() {
-  if (mouseIsPressed) {
-    stroke(255);
-    fill(255);
-    ellipse(mouseX, mouseY, 20, 20);
-    socket.emit("draw", mouseX, mouseY);
-  }
-  socket.on("draw", function(onmouseX, onmouseY) {
-    stroke(255);
-    fill(255);
-    ellipse(onmouseX, onmouseY, 20, 20);
-  });
-}
+var socket = io()
+document.querySelector('#plus').addEventListener('click', e => {
+  e.preventDefault()
+  socket.emit('increment')
+})
+document.querySelector('#minus').addEventListener('click', e => {
+  e.preventDefault()
+  socket.emit('decrement')
+})
+socket.on('increment', function(num) {
+  document.querySelector('#number').innerText = num
+})
+socket.on('decrement', function(num) {
+  document.querySelector('#number').innerText = num
+})
