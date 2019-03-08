8cd2ca489030b12db7f90eb17c99285020b0e516
CatsAreMLG <joshuadgon@gmail.com>
2019-03-08 01:15:40 -0600

    Add draw button when game is started

--- static/js/numberControl.js
+++ static/js/numberControl.js
@@ -4,15 +4,30 @@ document.querySelector('#start').addEventListener('click', e => {
   e.preventDefault()
   socket.emit('start')
 })
+const drawDiv = _ => {
+  let draw = document.createElement('button')
+  draw.id = 'draw'
+  draw.innerText = 'DRAW'
+  draw.addEventListener('click', e => {
+    e.preventDefault()
+    socket.emit('draw')
+  })
+  return draw
+}
 socket.on('start', function(deck) {
   console.log(deck)
   document.querySelector('.deck').innerText = ''
+  const draw = drawDiv()
   deck.map(card => {
     let div = document.createElement('div')
+    document.querySelector('.button_container').appendChild(draw)
     div.classList.add(
       card.suit == 'Diamonds' || card.suit == 'Hearts' ? 'red' : 'black'
     )
     div.innerText = `${card.number} of ${card.suit}`
     document.querySelector('.deck').appendChild(div)
   })
+})
+socket.on('draw', function(hand) {
+  console.log(hand)
 })
